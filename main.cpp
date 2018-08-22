ca7809796aa26e2fc915baf47eb8435c66225f28
Aki Sipovaara <mushut@gmail.com>
2018-08-22 13:32:32 +0300

    Wrote main.cpp little further

--- main.cpp
+++ main.cpp
@@ -3,10 +3,26 @@
  */
 
 #include <iostream>
+#include <ifstream>
+#include <string>
+#include "node.h"
 
-int main(void)
+// Graph data is read from a csv file given as an argument to the program.
+int main(int argc, char** argv)
 {
 	std::cout << "Graph" << std::endl;
+
+	std::ifstream input;
+	input.open(*(argv + 1), std::ifstream::in);
+
+	std::string line;
+	std::string delimiter = ",";
+	while (std::getline(input, line) {
+		Node newNode();
+
+		// Parse csv data from file. One row equals one node data.
+	}
+	input.close();
 
 	return 0;
 }
