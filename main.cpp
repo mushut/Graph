97b01da3b10dadbca7774377bafa226be3b5e5ff
Aki Sipovaara <mushut@gmail.com>
2018-08-23 16:46:22 +0300

    Progress

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <ifstream>
 #include <string>
+#include <sstream>
+#include <vector>
 #include "node.h"
 
 // Graph data is read from a csv file given as an argument to the program.
@@ -16,11 +18,22 @@ int main(int argc, char** argv)
 	input.open(*(argv + 1), std::ifstream::in);
 
 	std::string line;
+	std::stringstream stream;
 	std::string delimiter = ",";
+	std::vector<std::string> data;
+	std::string tempString;
 	while (std::getline(input, line) {
 		Node newNode();
 
 		// Parse csv data from file. One row equals one node data.
+		stream << line;
+		while (stream >> tempString) {
+			data.push_back(tempString);
+
+			if (stream.peek() == ',') {
+				stream.ignore();
+			}
+		}
 	}
 	input.close();
 
